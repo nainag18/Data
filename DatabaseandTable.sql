e3cf83b17b04f2101ecb8138a275b69abf7dcf07
nainag18 <naina.grewal18@gmail.com>
2025-06-04 19:49:55 -0400

    Create DatabaseandTable.sql

---
+++ DatabaseandTable.sql
@@ -0,0 +1,9 @@
+# Database Creation
+CREATE USER admin WITH PASSWORD 'admin';
+CREATE DATABASE test;
+GRANT ALL PRIVILEGES ON DATABASE test TO admin;
+
+# Table Creation 
+create table installs (country varchar(2), country_id integer, user_install_id UUID, client varchar(255), year integer, month integer, day integer);
+create table spend (country_id integer, client varchar(255), year integer, month integer, day integer, spend numeric(18,8));
+create table revenue (country varchar(2), client varchar(255), year integer, month integer, day integer, user_install_id UUID, revenue numeric(18,8));
