eb782c616fd7761b582ef56ae241480c7d142ce0
nainag18 <naina.grewal18@gmail.com>
2025-06-04 20:22:10 -0400

    Update DatabaseandTable.sql

--- DatabaseandTable.sql
+++ DatabaseandTable.sql
@@ -7,3 +7,9 @@ GRANT ALL PRIVILEGES ON DATABASE test TO admin;
 create table installs (country varchar(2), country_id integer, user_install_id UUID, client varchar(255), year integer, month integer, day integer);
 create table spend (country_id integer, client varchar(255), year integer, month integer, day integer, spend numeric(18,8));
 create table revenue (country varchar(2), client varchar(255), year integer, month integer, day integer, user_install_id UUID, revenue numeric(18,8));
+
+# Table Number of rows after importing csv files 
+installs - rows 81752
+revenue - rows 2126539
+spend - rows 102
+
