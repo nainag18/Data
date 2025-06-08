d8dbe47461d733a77b46116f9cabe4074eb99d6c
nainag18 <naina.grewal18@gmail.com>
2025-06-08 13:31:19 -0400

    Update DatabaseandTable.sql

--- DatabaseandTable.sql
+++ DatabaseandTable.sql
@@ -3,13 +3,14 @@ CREATE USER admin WITH PASSWORD 'admin';
 CREATE DATABASE test;
 GRANT ALL PRIVILEGES ON DATABASE test TO admin;
 
+# 2nd Option for Importing data into tables
 # Table Creation 
 create table installs (country varchar(2), country_id integer, user_install_id UUID, client varchar(255), year integer, month integer, day integer);
 create table spend (country_id integer, client varchar(255), year integer, month integer, day integer, spend numeric(18,8));
 create table revenue (country varchar(2), client varchar(255), year integer, month integer, day integer, user_install_id UUID, revenue numeric(18,8));
 
 # Table Number of rows after importing csv files 
-Steps Right click on table and select Import/Export Data.. and choose right file according to table
+Steps Right click on table and select Import/Export Data.. and choose right CSV file according to table
 installs - rows 81752
 revenue - rows 2126539
 spend - rows 102
